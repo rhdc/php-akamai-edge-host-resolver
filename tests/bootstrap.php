e5ed706e217bc68b5ed5452d6ecf0abdfb6256e9
Shawn Iwinski <siwinski@redhat.com>
2017-09-15 12:02:00 -0400

    Set up phpunit

---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of the RHDC Akamai edge host resolver package.
+ *
+ * (c) Shawn Iwinski <siwinski@redhat.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// Display all errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Autoloader
+require_once dirname(__DIR__).'/vendor/autoload.php';
