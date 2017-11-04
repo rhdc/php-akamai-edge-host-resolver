2bd4a69a86fdd80716e2bc4ec593a0888b459b7c
Shawn Iwinski <siwinski@redhat.com>
2017-11-04 16:17:50 -0400

    [Exception/*] Initial exception classes

---
+++ src/Exception/ResolveException.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This file is part of the RHDC Akamai edge resolver package.
+ *
+ * (c) Shawn Iwinski <siwinski@redhat.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Rhdc\Akamai\Edge\Resolver\Exception;
+
+class ResolveException extends \Exception
+{
+}
