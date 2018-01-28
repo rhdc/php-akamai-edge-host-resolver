7b431e0fa34c999ba83512cc6dbf78805c198a37
Shawn Iwinski <siwinski@redhat.com>
2018-01-28 14:50:07 -0500

    [NativeResolver] Use ResolverAbstract::RESULT_ITEM_KEY_* constants and add TTL

--- src/NativeResolver.php
+++ src/NativeResolver.php
@@ -55,21 +55,24 @@ class NativeResolver extends ResolverAbstract
         return $this->dnsGetRecord($host, $dnsGetRecordType);
     }
 
-    protected function resolveResultItemValue($resultItem, $resolve)
+    protected function resolveResultItemValue($resultItem, $resultItemKey)
     {
-        switch ($resolve) {
-            case ResolverInterface::RESOLVE_HOST:
+        switch ($resultItemKey) {
+            case static::RESULT_ITEM_KEY_HOST:
                 $resultItemKey = 'host';
                 break;
-            case ResolverInterface::RESOLVE_IP_V4:
+            case static::RESULT_ITEM_KEY_IP_V4:
                 $resultItemKey = 'ip';
                 break;
-            case ResolverInterface::RESOLVE_IP_V6:
+            case static::RESULT_ITEM_KEY_IP_V6:
                 $resultItemKey = 'ipv6';
+                break;
+            case static::RESULT_ITEM_KEY_TTL:
+                $resultItemKey = 'ttl';
                 break;
             default:
                 throw new ResolveException(sprintf(
-                    'Invalid resolve type: "%s"',
+                    'Invalid result item key: "%s"',
                     $resolve
                 ));
         }
