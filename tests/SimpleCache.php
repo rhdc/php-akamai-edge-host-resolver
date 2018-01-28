c0aa98701ee817582f3324e6b76f5187b1bc5da0
Shawn Iwinski <siwinski@redhat.com>
2018-01-28 14:52:24 -0500

    [Test\SimpleCache] Add PSR simple cache implementation test class

---
+++ tests/SimpleCache.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * This file is part of the RHDC Akamai edge resolver package.
+ *
+ * (c) Shawn Iwinski <siwinski@redhat.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+namespace Rhdc\Akamai\Edge\Resolver\Test;
+
+use Psr\SimpleCache\CacheInterface;
+use RuntimeException;
+
+class SimpleCache implements CacheInterface
+{
+    /** @var array */
+    protected $data = array();
+
+    /** @var array */
+    protected $ttl = array();
+
+    public function count()
+    {
+        return count($this->data);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($key, $default = null) {
+        return isset($this->data[$key]) ? $this->data[$key] : $default;
+    }
+
+    public function getTtl($key) {
+        return isset($this->ttl[$key]) ? $this->ttl[$key] : null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function set($key, $value, $ttl = null) {
+        $this->data[$key] = $value;
+        $this->ttl[$key] = $ttl;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete($key) {
+        throw new RuntimeException('Not implemented');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function clear() {
+        throw new RuntimeException('Not implemented');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMultiple($keys, $default = null) {
+        throw new RuntimeException('Not implemented');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setMultiple($values, $ttl = null) {
+        throw new RuntimeException('Not implemented');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteMultiple($keys) {
+        throw new RuntimeException('Not implemented');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function has($key) {
+        throw new RuntimeException('Not implemented');
+    }
+}
